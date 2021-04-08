c3cfec0a456dd2cda54c0aa1ac500ba75f84b4e9
Sasikumar Arumugam <sasikumar.arumugam@syncfusion.com>
2021-04-08 14:21:46 +0530

    FLUT-4313-Sample updated with null safety.

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,6 @@ class MonthFormat extends StatelessWidget {
             ),
           ),
         ),
-
-        // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
